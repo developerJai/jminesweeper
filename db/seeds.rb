1211a2031ef4b22bfa57dda79f608602d21eb139
Jai <jai@tecorb.co>
2023-10-19 15:18:47 +0530

    Seed updated for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,13 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-users = ["jai@tecorb.com", "jai@tecorb.co", "rama@tecorb.co", "rishabh@tecorb.co"]
-mat = [[10,10, 10], [5,5, 10], [5,3,8]]
-users.each_with_index do |email, ind|
-  user = User.where(email: email).first_or_create
-  data = mat.sample
-  board = user.boards.where(name: Faker::Artist.name, width: data[0], height: data[1], mines: data[2]).first_or_create
-  board.
+users = ["jai@tecorb.com", "jai@tecorb.co", "rama@tecorb.co", "rishabh@tecorb.co", "josh@example.com", "test@sample.com"]
+1.upto(3).each do
+  mat = [[10,10, 10], [5,5, 10], [5,3,8]]
+  users.each_with_index do |email, ind|
+    user = User.where(email: email).first_or_create
+    data = mat.sample
+    board = user.boards.where(name: Faker::Artist.name, width: data[0], height: data[1], mines: data[2]).first_or_create
+    board.generate_minesweeper_board
+  end
 end
